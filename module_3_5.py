1d77865a15f95d2d52c63bf48f140c8373d404df
Loara999 <54015247+Loara999@users.noreply.github.com>
2024-09-28 19:59:07 +0500

    module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -1,10 +1,12 @@
 def get_multiplied_digits(number):
     str_number = str(number)
     first = int(str_number[0])
-    if len(str_number) > 1:
+    if len(str_number) > 1 and first != 0:
         return first * get_multiplied_digits((int(str_number[1:])))
-    else:
+    elif first != 0:
         return first
+    else:
+        return 1
 
 
-print(get_multiplied_digits(50803))
+print(get_multiplied_digits(42000))
